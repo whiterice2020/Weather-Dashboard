e5816a5563dd51a9e710ad2ceda96290585895da
Paul DelPadre <PDelPadre@rice.local>
2020-06-23 01:38:00 -0400

    Updating Code

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,16 @@
 // example "api.openweathermap.org/data/2.5/forecast?q= "City Var" "apiKey Var"{your api key}"
 // var queryURL = "api.openweathermap.org/data/2.5/forecast?q=";
 // test var for orlando full url
-queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=Orlando&appid=eee27dfad5db9dce473a5293caa3df71";
+// queryURLCurrent = "api.openweathermap.org/data/2.5/weather?q= {city name} &appid= {your api key} ";
+queryURLCurrent = "https://api.openweathermap.org/data/2.5/weather?q=Orlando&appid=eee27dfad5db9dce473a5293caa3df71";
 queryURL5Day = "https://api.openweathermap.org/data/2.5/forecast?q=Orlando&appid=eee27dfad5db9dce473a5293caa3df71";
+// queryURLUV = "http://api.openweathermap.org/data/2.5/uvi?appid= **api key** &lat=  **{lat}**  &lon=  **{lon}**";
+// queryURLUV = "http://api.openweathermap.org/data/2.5/uvi?appid= **api key** &lat=  **  &lon=  **Lon**";
+queryURLUV = "http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}";
 var apiKey = "&appid=eee27dfad5db9dce473a5293caa3df71";
-var city =
+var city ="";
+var lat = "";
+var lon = "";
 
 
 
@@ -14,20 +20,20 @@ var city =
 
 // Getting Data from API Current Weather
 $.ajax({
-    url: queryURL,
+    url: queryURLCurrent,
     method: "GET"
   }).then(function(response) {
-//     var temp = response.list[0].main.temp;
-//     var humidty = response.list[0].main.humidity;
-//     var windSpeed = response.list[0].wind.speed;
+    var temp = response.main.temp;
+    var humidty = response.main.humidity;
+    var windSpeed = response.wind.speed;
 //     // var uvIndex = response.main.temp; have to fix this
 // // Testing Response Data
-//     console.log("temp" + temp);
-//     console.log("humidity" + humidty);
-//     console.log("Windspeed" + windSpeed);
+    console.log("Current temp " + temp);
+    console.log("Current humidity " + humidty);
+    console.log("Current Windspeed " + windSpeed);
     
 
-
+  });
 
 // Getting Data from API for 5 day forcast
 $.ajax({
@@ -39,15 +45,19 @@ $.ajax({
     var windSpeed = response.list[0].wind.speed;
     // var uvIndex = response.main.temp; have to fix this
 // Testing Response Data
-    console.log("temp" + temp);
-    console.log("humidity" + humidty);
-    console.log("Windspeed" + windSpeed);
+    console.log("temp " + temp);
+    console.log("humidity " + humidty);
+    console.log("Windspeed " + windSpeed);
     
 
     
     
   });
 
+$(".button").on("click", function(){
+// code to capture text from input box and save as var city
+
+});
 
 // ✓ User can search for weather reports by city using the openweathermap API.
 
